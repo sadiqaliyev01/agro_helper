56bc41619dc9b994f6425214d6c1bd0fbc5efc6f
Farid023 <gurbanoff.farid@gmail.com>
2024-04-30 21:47:33 +0400

    Added Profile Screen

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
+class AppColors {
   AppColors._();
 
-  static Color pink = const Color(0xffFF43B7);
-  static Color turquoise = const Color(0xff42AB9C);
-  static Color green = const Color(0xff193c0d);
-  static Color white = Colors.white;
-  static Color brown = const Color(0xff958934);
-  static Color screenBackground = const Color(0xffefebe5);
-  static Color brown2 = const Color(0xff26130b);
-  static Color selectedIcon = const Color(0xffd1d8bd);
-  static Color black = Colors.black;
+  static const pink = Color(0xffFF43B7);
+  static const turquoise = Color(0xff42AB9C);
+  static const green = Color(0xff193c0d);
+  static const white = Colors.white;
+  static const brown = Color(0xff958934);
+  static const profileButtonsBackground = Color(0xffbfcca3);
+  static const screenBackground = Color(0xffefebe5);
+  static const brown2 = Color(0xff26130b);
+  static const selectedIcon = Color(0xffd1d8bd);
+  static const black = Colors.black;
 }
--- lib/ui/presentation/profile/widgets/profile_view.dart
+++ lib/ui/presentation/profile/widgets/profile_view.dart
@@ -1,14 +1,62 @@
-import 'package:flutter/cupertino.dart';
+import 'package:agro_helper/constants/app_colors.dart';
+import 'package:agro_helper/ui/presentation/profile/widgets/profile_element_buttons.dart';
+import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text("PROFIL"),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: CircleAvatar(
+                radius: 64,
+                backgroundColor: AppColors.profileButtonsBackground,
+                child: Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/jpeg_images/avatar_blank.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ProfileElementButtons(
+            title: "Settings",
+            icon: Icons.settings,
+            onTap: () {},
+          ),
+          const SizedBox(height: 12),
+          ProfileElementButtons(
+            title: "Privacy Policy",
+            icon: Icons.privacy_tip_outlined,
+            onTap: () {},
+          ),
+          const SizedBox(height: 12),
+          ProfileElementButtons(
+            title: "Log out",
+            icon: Icons.logout,
+            onTap: () {},
+          ),
+          const SizedBox(height: 30),
+          ProfileElementButtons(
+            title: "Delete account",
+            icon: Icons.clear,
+            onTap: () {},
+            foregroundColor: Colors.redAccent,
+          ),
+        ],
+      ),
     );
   }
 }
